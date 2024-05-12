347de4667467d9402c5573de2ea7b4b4b40c903f
Raymond Hill <rhill@raymondhill.net>
2024-05-12 14:17:31 -0400

    add release workflow

--- tools/make-chromium.sh
+++ tools/make-chromium.sh
@@ -16,4 +16,14 @@ bash ./tools/copy-common-files.sh $DES
 echo "*** jsawpuzzle.chromium: Copying chromium-specific files"
 cp platform/chromium/*.json $DES/
 
+if [ -n "$1" ]; then
+    echo "*** jsawpuzzle.chromium: Patching manifest.json"
+    tmp=$(mktemp)
+    jq --arg a "$1" '.version = $a' "$DES/manifest.json" > "$tmp" && mv "$tmp" "$DES/manifest.json"
+    echo "*** jsawpuzzle.chromium: Creating versioned package"
+    pushd $(dirname $DES/) > /dev/null
+    zip jsawpuzzle_"$1".chromium.zip -qr $(basename $DES/)/*
+    popd > /dev/null
+fi
+
 echo "*** jsawpuzzle.chromium: Package done."
--- tools/make-firefox.sh
+++ tools/make-firefox.sh
@@ -16,4 +16,15 @@ bash ./tools/copy-common-files.sh $DES
 echo "*** jsawpuzzle.firefox: Copying chromium-specific files"
 cp platform/firefox/*.json $DES/
 
+if [ -n "$1" ]; then
+    echo "*** jsawpuzzle.firefox: Patching manifest.json"
+    tmp=$(mktemp)
+    jq --arg a "$1" '.version = $a' "$DES/manifest.json" > "$tmp" && mv "$tmp" "$DES/manifest.json"
+    echo "*** jsawpuzzle.firefox: Creating versioned package..."
+    pushd $DES > /dev/null
+    zip ../$(basename $DES).xpi -qr *
+    popd > /dev/null
+    mv build/jsawpuzzle.firefox.xpi build/jsawpuzzle_"$1".firefox.xpi
+fi
+
 echo "*** jsawpuzzle.firefox: Package done."
